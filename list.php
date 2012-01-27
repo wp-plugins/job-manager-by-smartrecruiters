eb73123f4fbb5f96b3b79b501292bf8ed6f21057
SmartRecruiters <SmartRecruiters@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-27 13:52:46 +0000

    Version 1.0.7 * Minor changes
    git-svn-id: https://plugins.svn.wordpress.org/job-manager-by-smartrecruiters/tags/1.0.7@496150 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ list.php
@@ -0,0 +1,76 @@
+<link rel="stylesheet" href="<?php echo plugins_url('style.css', __FILE__); ?>" type="text/css" />
+
+<?php if(isset($jobs['jobs']['job']) && count($jobs['jobs']['job']) && $jobs['jobs']['job']): ?>
+
+	<?php $pos = strpos($_SERVER["REQUEST_URI"], '?'); ?>
+	
+	<!-- SmartRecruiters Jobs List -->
+	
+	<ul class="smartrecruitersJobList">
+		<?php if(isset($jobs['jobs']['job']['@attributes'])): ?>
+		
+			<li class="smartrecruitersJobListElement">
+				<h2 class="smartrecruitersJobListElementHeader">
+			
+					<?php if($pos === false): ?>
+						<a href="<?php echo $_SERVER["REQUEST_URI"].'/srjob/'.$jobs['jobs']['job']['@attributes']['code']; ?>" title="<?php echo $jobs['jobs']['job']['title']; ?>"><?php echo $jobs['jobs']['job']['title']; ?><?php echo $jobs['jobs']['job']['title']; ?></a>
+				
+						<?php else: ?>
+					
+							<a href="<?php echo $guid.'&srjob='.$jobs['jobs']['job']['@attributes']['code']; ?>" title="<?php echo $jobs['jobs']['job']['title']; ?>"><?php echo $jobs['jobs']['job']['title']; ?><?php echo $jobs['jobs']['job']['title']; ?></a>
+						
+						<?php endif; ?>
+						
+				</h2>
+								
+				<?php if(isset($jobs['jobs']['job']['job-location']['city']) && $jobs['jobs']['job']['job-location']['city']): ?>
+					<ul class="smartrecruitersJobListDetails">
+						<li class="smartrecruitersJobListDetailsElement"><?php echo $jobs['jobs']['job']['job-location']['city']; ?></li>
+					</ul>
+				<?php endif; ?>
+			
+			</li>
+			
+		<?php else: ?>
+		
+			<?php foreach($jobs['jobs']['job'] as $job): ?>
+
+				<?php if((count($locations) && in_array($job['job-location']['city'], $locations)) || !count($locations)): ?>
+					
+					<?php if((count($departments) && in_array($job['department'], $departments)) || !count($departments)): ?>
+				
+						<li class="smartrecruitersJobListElement">
+
+							<h2 class="smartrecruitersJobListElementHeader">
+								
+								<?php if($pos === false): ?>
+									<a href="<?php echo $_SERVER["REQUEST_URI"].'/srjob/'.$job['@attributes']['code']; ?>" title="<?php echo $job['title']; ?>"><?php echo $job['title']; ?></a>
+				
+								<?php else: ?>
+									<a href="<?php echo $guid.'&srjob='.$job['@attributes']['code']; ?>" title="<?php echo $jobs['title']; ?>"><?php echo $job['title']; ?></a>
+						
+								<?php endif; ?>
+						
+					</h2>
+								
+								<?php if(isset($job['job-location']['city']) && $job['job-location']['city']): ?>
+									<ul class="smartrecruitersJobListDetails">
+										<li class="smartrecruitersJobListDetailsElement"><?php echo $job['job-location']['city']; ?></li>
+									</ul>
+								<?php endif; ?>
+								
+							</li>
+					
+					<?php endif; ?>
+					
+				<?php endif; ?>
+				
+			<?php endforeach; ?>
+			
+		<?php endif; ?>
+		
+	</ul>
+	
+	<!-- /SmartRecruiters Jobs List -->
+	
+<?php endif; ?>
